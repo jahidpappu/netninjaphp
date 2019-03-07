4709adf72877d7bf46802329fad41da3099a7df6
jahid <jahidpappublog@gmail.com>
2019-03-07 09:50:08 +0600

    new

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 
 <?php 
-$title = $email = $Ingredients= '';
-$errors = array('email'=>'', 'title'=>'','Ingredients'=>'');
+$title = $email = $ingredients = '';
+$errors = array('email'=>'', 'title'=>'','ingredients'=>'');
 
 
     // if(isset($_GET['submit'])){
@@ -13,45 +13,26 @@ $errors = array('email'=>'', 'title'=>'','Ingredients'=>'');
     if(isset($_POST['submit'])){
        // echo htmlspecialchars($_POST['email']);
        // echo htmlspecailchars($_POST['title']);
-       // echo htmlspecialchars($_POST['Ingredients']);
+       // echo htmlspecialchars($_POST['ingredients']);
 
        //check email
-       if(empty($_POST['email'])){
-           $errors['email'] = 'An email is required <br/>';
-       }else{
-           $email = $_POST['email'];
-          if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $errors['email'] = 'email must be a valid email address';
-          }
-       }
-
-        //check title
-        if(empty($_POST['title'])){
-            $errors['title'] = 'An title is required <br/>';
-       }else{
-           $title = $_POST['title'];
-           if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-            $errors['title'] = 'Title must be letters and spaces only';
-          }
-       }
-
-    
-        //check Ingredients
-        if(empty($_POST['Ingredients'])){
-            $errors['Ingredients'] = 'An Ingredients is required <br/>';
-       }else{
-           $Ingredients = $_POST['Ingredients'];
-           if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $Ingredients)){
-            $errors['Ingredients'] = 'Ingredients must be letters and spaces only';
-          }
-       }
-
-       if( array_filter($errors)){
-            echo 'errors in the form';
-       }else{
-           echo 'form is valid';
-           header('Location: index.php');
-       }
+      if(empty($_POST['email'])){
+        echo 'An email is required <br/>';
+      }else{
+          echo htmlspecialchars($_POST['email']);
+      }
+      //check email
+      if(empty($_POST['title'])){
+        echo 'An title is required <br/>';
+      }else{
+          echo htmlspecialchars($_POST['title']);
+      }
+      //check ingredients
+      if(empty($_POST['ingredients'])){
+        echo 'An ingredients is required <br/>';
+      }else{
+          echo htmlspecialchars($_POST['ingredients']);
+      }
 
        
     }
@@ -74,8 +55,8 @@ $errors = array('email'=>'', 'title'=>'','Ingredients'=>'');
         <input type="text" name="title" value = "<?php echo htmlspecialchars($title); ?>">
         <div class="red-text"><?php $errors['title']  ?></div>
         <label>Ingredients(comma separated):</label>
-        <input type="text" name="Ingredients" value = "<?php echo htmlspecialchars($Ingredients); ?>">
-        <div class="red-text"><?php $errors['Ingredients']  ?></div>
+        <input type="text" name="ingredients" value = "<?php echo htmlspecialchars($ingredients); ?>">
+        <div class="red-text"><?php $errors['ingredients']  ?></div>
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
